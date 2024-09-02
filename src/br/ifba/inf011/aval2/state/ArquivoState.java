27369b794357f83b24a420039987e25801e7bcb1
CarolLimav <carol.lima.vitoria@gmail.com>
2024-09-01 23:13:09 -0300

    last commit

--- src/br/ifba/inf011/aval2/state/ArquivoState.java
+++ src/br/ifba/inf011/aval2/state/ArquivoState.java
@@ -4,21 +4,13 @@ package br.ifba.inf011.aval2.state;
 public interface ArquivoState {
 	
 	public String setConteudo(String conteudo) throws  IllegalAccessException;
-	
 	ArquivoState restaurar() throws IllegalAccessException;
-
 	ArquivoState bloquear() throws IllegalAccessException;
-
 	ArquivoState definirSomenteLeitura() throws IllegalAccessException;
-
 	ArquivoState excluir() throws IllegalAccessException;
-    
 	ArquivoState liberar() throws IllegalAccessException;
-
     public Long getTamanho() throws IllegalAccessException;
-    
     public String descricao() throws IllegalAccessException;
-    
     String ler() throws IllegalAccessException;
 
 }
